966cdbf29d855abf6f1e5caf4d7dbde9a848331e
Daniel Richman <main@danielrichman.co.uk>
2014-08-10 13:20:08 +0100

    ext_modules should be a list

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@ import sys
 from setuptools import setup, Extension
 
 try:
-     from Cython.Build import cythonize
-     cython_present = True
-except ImportError:
-     cython_present = False
+    from Cython.Build import cythonize
 
-if cython_present:
     PY2 = sys.version_info[0] == 2
     ext_modules = cythonize("magicmemoryview.pyx",
                             compile_time_env={'PY2': PY2},
                             force=True)
-else:
-    ext_modules = Extension('magicmemoryview', ['magicmemoryview.c'])
+
+except ImportError:
+    ext_modules = [Extension('magicmemoryview', ['magicmemoryview.c'])]
 
 with open('README.rst') as f:
     long_description = f.read()
 
 setup(
     name="magicmemoryview",
-    version="0.1.4",
+    version="0.1.5",
     author='Cambridge University Spaceflight',
     author_email='contact@cusf.co.uk',
     url='http://github.com/cuspaceflight/magicmemoryview',
