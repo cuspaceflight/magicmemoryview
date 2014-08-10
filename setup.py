1025554a853203c63cc54df54f1bc6edb8c4ca6e
Daniel Richman <main@danielrichman.co.uk>
2014-08-10 13:03:39 +0100

    SDist by bundling .c

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 import sys
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import setup, Extension
 
-PY2 = sys.version_info[0] == 2
+try:
+     from Cython.Build import cythonize
+     cython_present = True
+except ImportError:
+     cython_present = False
+
+if cython_present:
+    PY2 = sys.version_info[0] == 2
+    ext_modules = cythonize("magicmemoryview.pyx",
+                            compile_time_env={'PY2': PY2})
+else:
+    ext_modules = Extension('magicmemoryview', ['magicmemoryview.c'])
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -12,14 +22,11 @@ setup(
     version="0.1.3",
     author='Cambridge University Spaceflight',
     author_email='contact@cusf.co.uk',
-    ext_modules = cythonize("magicmemoryview.pyx", compile_time_env={'PY2': PY2}),
     url='http://github.com/cuspaceflight/magicmemoryview',
     license='GPLv3+',
     description='Magic memoryview() style casting for Cython',
     long_description=long_description,
-    install_requires=[
-        "Cython",
-    ],
+    ext_modules=ext_modules,
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
